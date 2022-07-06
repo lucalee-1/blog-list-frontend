a5fa3e89fab3c27464b1f194f3149ceb49c73797
Luca Lee <30341309+lucalee-1@users.noreply.github.com>
2022-07-06 18:23:20 +0900

    Add main tag to Blog component

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -6,12 +6,12 @@ const Blogs = () => {
   const blogs = useSelector((state) => state.blogs);
   let sortedBlogs = blogs?.slice().sort((a, b) => b.likes - a.likes);
   return (
-    <>
+    <main>
       <h3>Blog List</h3>
       {sortedBlogs.map((blog) => (
         <BlogItem key={blog.id} blog={blog} user={user} />
       ))}
-    </>
+    </main>
   );
 };
 export default Blogs;
