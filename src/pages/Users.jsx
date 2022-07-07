ea189430c85a427c54d49a16d96596feadfe3573
Luca Lee <30341309+lucalee-1@users.noreply.github.com>
2022-07-07 21:23:01 +0900

    Style Users page

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,32 +1,52 @@
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import {
+  Typography,
+  Table,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell,
+  TableContainer,
+  styled,
+} from '@mui/material';
 
 const Users = () => {
-  const users = useSelector((state) => state.users);
+  const users = useSelector((state) => state.users?.filter((user) => user.blogs.length > 0));
+  const sortedUsers = users?.slice().sort((a, b) => b.blogs.length - a.blogs.length);
+
   if (!users) {
     return null;
   }
+
+  const CenteredCell = styled(TableCell)({ textAlign: 'center' });
   return (
     <>
-      <h3>Users</h3>
-      <table>
-        <thead>
-          <tr>
-            <th></th>
-            <th>Blogs added</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user) => (
-            <tr key={user.id}>
-              <td>
-                <Link to={`/users/${user.id}`}>{user.name}</Link>
-              </td>
-              <td>{user.blogs.length}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <Typography align="center" variant="h3" component="h3" sx={{ marginTop: 5, marginBottom: 5 }}>
+        Users
+      </Typography>
+      <TableContainer
+        sx={{ marginTop: 5, marginBottom: 5, display: 'flex', justifyContent: 'center' }}
+      >
+        <Table sx={{ width: '70%' }}>
+          <TableHead>
+            <TableRow>
+              <CenteredCell>User Name</CenteredCell>
+              <CenteredCell>Blogs Added</CenteredCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {sortedUsers.map((user) => (
+              <TableRow key={user.id}>
+                <CenteredCell>
+                  <Link to={`/users/${user.id}`}>{user.name}</Link>
+                </CenteredCell>
+                <CenteredCell>{user.blogs.length}</CenteredCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </>
   );
 };
