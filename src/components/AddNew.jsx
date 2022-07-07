4483d47591aa322c567c4d2597cc9365619c716e
Luca Lee <30341309+lucalee-1@users.noreply.github.com>
2022-07-07 20:43:39 +0900

    User must log in to add new blogs

--- src/components/AddNew.jsx
+++ src/components/AddNew.jsx
@@ -1,26 +1,49 @@
 import { useState } from 'react';
-import { Fab, Tooltip } from '@mui/material';
+import { useSelector } from 'react-redux';
+import { Fab, Tooltip, Popover, Typography } from '@mui/material';
 import { Add as AddIcon } from '@mui/icons-material';
 import DialogWrap from './DialogWrap';
 import NewBlogForm from './NewBlogForm';
 
 const AddNew = () => {
-  const [openDialog, setOpenDialog] = useState(false);
+  const [open, setOpen] = useState(null);
+  const loggedUser = useSelector((state) => state.login);
 
+  const handleClick = (e) => {
+    setOpen(e.currentTarget);
+  };
   return (
     <>
       <Tooltip
         title="Add New Blog"
-        onClick={() => setOpenDialog(true)}
+        onClick={handleClick}
         sx={{ position: 'fixed', bottom: 40, right: { xs: 'calc(50% - 25px)', sm: 40 } }}
       >
         <Fab color="primary">
           <AddIcon />
         </Fab>
       </Tooltip>
-      <DialogWrap title="Add New Blog" open={openDialog} setOpen={setOpenDialog}>
-        <NewBlogForm />
-      </DialogWrap>
+      {!loggedUser ? (
+        <Popover
+          open={Boolean(open)}
+          anchorEl={open}
+          onClose={() => setOpen(null)}
+          anchorOrigin={{
+            vertical: 'top',
+            horizontal: 'center',
+          }}
+          transformOrigin={{
+            vertical: 'bottom',
+            horizontal: 'right',
+          }}
+        >
+          <Typography sx={{ p: 2 }}>Log In to add new blogs!</Typography>
+        </Popover>
+      ) : (
+        <DialogWrap title="Add New Blog" open={open} setOpen={setOpen}>
+          <NewBlogForm />
+        </DialogWrap>
+      )}
     </>
   );
 };
