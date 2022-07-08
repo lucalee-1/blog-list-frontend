7a9040c4996fb4e9895df95ca3deccb9f7065cda
Luca Lee <30341309+lucalee-1@users.noreply.github.com>
2022-07-08 13:05:25 +0900

    mend

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,6 +1,6 @@
 import { useSelector } from 'react-redux';
 import { useParams, Link } from 'react-router-dom';
-import { Typography, List, ListItem, ListItemButton, Box, Link as MuiLink } from '@mui/material';
+import { Typography, List, ListItem, ListItemButton, Box } from '@mui/material';
 
 const User = () => {
   const id = useParams().id;
@@ -19,8 +19,8 @@ const User = () => {
         </Typography>
         <List>
           {user.blogs.map((blog) => (
-            <ListItem key={blog.id}>
-              <ListItemButton component={Link} to={`/blogs/${blog.id}`}>
+            <ListItem key={blog.id} divider>
+              <ListItemButton component={Link} to={`/blogs/${blog.id}`} sx={{ color: '#93B5C6' }}>
                 {blog.title}
               </ListItemButton>
             </ListItem>
